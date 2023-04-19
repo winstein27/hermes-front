5732d4b75aa91214efe2e3200bef84a006b3583e
Winstein Martins <sriwinstein@gmail.com>
2023-04-19 10:26:55 -0300

    :recycle: refactor: improving validation
    Changing zod validation function

--- src/components/login/LoginForm.tsx
+++ src/components/login/LoginForm.tsx
@@ -10,9 +10,9 @@ import Button from '@mui/material/Button';
 import FormInput from '../UI/FormInput';
 
 const loginSchema = object({
-  email: string().min(1, 'Email is required').email('Email is invalid'),
+  email: string().nonempty('Email is required').email('Email is invalid'),
   password: string()
-    .min(1, 'Password is required')
+    .nonempty('Password is required')
     .min(8, 'Password must be at least 8 characters')
     .max(32, 'Password must be less than 32 characters'),
 });
