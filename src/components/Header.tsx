c800274152547ef167128a9f6a7abb388a588a9b
Winstein Martins <sriwinstein@gmail.com>
2023-04-18 22:32:59 -0300

    :sparkles: feat: Login link
    Routing to login page when login button is clicked.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,5 @@
+import { useRouter } from 'next/router';
+
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -7,6 +9,8 @@ import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 
 const Header = () => {
+  const router = useRouter();
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
@@ -23,7 +27,13 @@ const Header = () => {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             News
           </Typography>
-          <Button color="inherit">Login</Button>
+          <Button
+            color="inherit"
+            type="button"
+            onClick={() => router.push('/login')}
+          >
+            Login
+          </Button>
         </Toolbar>
       </AppBar>
     </Box>
